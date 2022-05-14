6f84286f043934bfd3d1b744b9f9934d8767e88e
HassanBahati <mukisabahati@gmail.com>
2022-05-14 13:09:54 +0300

    add layout structure

--- components/Layout.js
+++ components/Layout.js
@@ -1,2 +1,13 @@
-t add .
-git commit 
+import React from 'react'
+
+export default function Layout({children}) {
+  return (
+    <>
+    <div>
+        <header>header</header>
+        <main>{children}</main>
+        <footer>footer</footer>
+    </div>
+    </>
+  )
+}
