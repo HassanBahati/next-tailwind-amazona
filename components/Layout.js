64187d7a344ac3a0bfac33dcf1839b42d1501731
HassanBahati <mukisabahati@gmail.com>
2022-05-14 13:34:28 +0300

    create nav bar

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Head from "next/head";
+import Link from "next/link";
 
-export default function Layout({ children ,title}) {
+export default function Layout({ children, title }) {
   return (
     <>
       <Head>
@@ -10,7 +11,17 @@ export default function Layout({ children ,title}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex min-h-screen flex-col justify-between">
-        <header className="">header</header>
+        <header className="">
+          <nav className="flex h-12 justify-between">
+            <Link href="/">
+              <a className="text-lg font-bold">amazona</a>
+            </Link>
+            <div>
+              <Link href="/cart">Cart</Link>
+              <Link href="login">Login</Link>
+            </div>
+          </nav>
+        </header>
         <main>{children}</main>
         <footer>footer</footer>
       </div>
