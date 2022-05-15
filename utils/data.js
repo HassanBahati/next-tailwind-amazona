d26b505a6d8a33fc7ab400364bfa6b5dba596a33
HassanBahati <mukisabahati@gmail.com>
2022-05-15 11:12:49 +0300

    add products

--- utils/data.js
+++ utils/data.js
@@ -1,10 +1,76 @@
-const data ={
-    products:[
-        {
-            name:"Free Shirt",
-            slug:"free-shirt",
-            category:"Shirts",
-            
-        }
-    ]
-}
+const data = {
+  products: [
+    {
+      name: "Free Shirt",
+      slug: "free-shirt",
+      category: "Shirts",
+      image: "/images/shirt1.jpg",
+      price: 7000,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: "A popular shirt",
+    },
+    {
+      name: "Fit Shirt",
+      slug: "fit-shirt",
+      category: "Shirts",
+      image: "/images/shirt2.jpg",
+      price: 8000,
+      brand: "Adidas",
+      rating: 3.2,
+      numReviews: 10,
+      countInStock: 20,
+      description: "A popular shirt",
+    },
+    {
+      name: "Slim Shirt",
+      slug: "slim-shirt",
+      category: "Shirts",
+      image: "/images/shirt3.jpg",
+      price: 9000,
+      brand: "Raymond",
+      rating: 1.5,
+      numReviews: 3,
+      countInStock: 20,
+      description: "A popular shirt",
+    },
+    {
+      name: "Golf Pants",
+      slug: "golf-pants",
+      category: "Pants",
+      image: "/images/pants1.jpg",
+      price: 2000,
+      brand: "Oliver",
+      rating: 2.9,
+      numReviews: 13,
+      countInStock: 20,
+      description: "Smart loooking pants",
+    },
+    {
+      name: "Fit Pants",
+      slug: "fit-pants",
+      category: "Pants",
+      image: "/images/pants2.jpg",
+      price: 9500,
+      brand: "Zara",
+      rating: 3.5,
+      numReviews: 7,
+      countInStock: 20,
+      description: "A popular pants",
+    },
+    {
+      name: "Classic Pants",
+      slug: "classic-pants",
+      category: "Pants",
+      image: "/images/pants3.jpg",
+      price: 7500,
+      brand: "Casely",
+      rating: 2.4,
+      numReviews: 14,
+      countInStock: 20,
+      description: "A popular pants",
+    },
+  ],
+};
