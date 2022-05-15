0e3fe4b90bf35a80955c042cd7c321ee0df2a7b3
HassanBahati <mukisabahati@gmail.com>
2022-05-15 11:40:58 +0300

    fix bug

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,10 @@ export default function Home() {
   return (
     <Layout title="Home Page">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-        {data.map((product) => (
+        {data.products.map((product) => (
           <ProductItem product={product} key={product.slug} />
         ))}
       </div>
-    </Layout>
+    </Layout> 
   );
 }
