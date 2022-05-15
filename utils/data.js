11a137da005f29f44af43ecdf94c7ebb580391c6
HassanBahati <mukisabahati@gmail.com>
2022-05-15 11:20:41 +0300

    add image links to data object

--- utils/data.js
+++ utils/data.js
@@ -4,7 +4,8 @@ const data = {
       name: "Free Shirt",
       slug: "free-shirt",
       category: "Shirts",
-      image: "/images/shirt1.jpg",
+      image:
+        "https://images.unsplash.com/photo-1603252109303-2751441dd157?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
       price: 7000,
       brand: "Nike",
       rating: 4.5,
@@ -16,7 +17,8 @@ const data = {
       name: "Fit Shirt",
       slug: "fit-shirt",
       category: "Shirts",
-      image: "/images/shirt2.jpg",
+      image:
+        "https://images.unsplash.com/photo-1596755094514-f87e34085b2c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80",
       price: 8000,
       brand: "Adidas",
       rating: 3.2,
@@ -28,7 +30,8 @@ const data = {
       name: "Slim Shirt",
       slug: "slim-shirt",
       category: "Shirts",
-      image: "/images/shirt3.jpg",
+      image:
+        "https://images.unsplash.com/photo-1618354691373-d851c5c3a990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=415&q=80",
       price: 9000,
       brand: "Raymond",
       rating: 1.5,
@@ -40,7 +43,8 @@ const data = {
       name: "Golf Pants",
       slug: "golf-pants",
       category: "Pants",
-      image: "/images/pants1.jpg",
+      image:
+        "https://images.unsplash.com/photo-1541099649105-f69ad21f3246?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
       price: 2000,
       brand: "Oliver",
       rating: 2.9,
@@ -52,7 +56,8 @@ const data = {
       name: "Fit Pants",
       slug: "fit-pants",
       category: "Pants",
-      image: "/images/pants2.jpg",
+      image:
+        "https://images.unsplash.com/photo-1594633312681-425c7b97ccd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
       price: 9500,
       brand: "Zara",
       rating: 3.5,
@@ -64,7 +69,8 @@ const data = {
       name: "Classic Pants",
       slug: "classic-pants",
       category: "Pants",
-      image: "/images/pants3.jpg",
+      image:
+        "https://images.unsplash.com/photo-1624378439575-d8705ad7ae80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1394&q=80",
       price: 7500,
       brand: "Casely",
       rating: 2.4,
